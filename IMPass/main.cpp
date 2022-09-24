bd178b3f56a7ab7d5000540b57501e63fab33bf1
viwas45hu978ot <99822720+viwas45hu978ot@users.noreply.github.com>
2022-09-24 10:09:04 +0200

    license

--- IMPass/main.cpp
+++ IMPass/main.cpp
@@ -1,3 +1,13 @@
+/*
+Copyright(C) 2022 viwas45hu978ot
+
+This program is free software; you can redistribute itand /or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see http ://www.gnu.org/licenses/gpl-3.0
+*/
+
 #include <iostream>
 #include "passgen.h"
 #include "main.h"
