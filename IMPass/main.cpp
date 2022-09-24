73ef808b8b4b78619b0c00c64a742a06715c9277
viwas45hu978ot <99822720+viwas45hu978ot@users.noreply.github.com>
2022-09-24 09:59:31 +0200

    Dear IMGUI implementation

--- IMPass/main.cpp
+++ IMPass/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include "passgen.h"
+#include "main.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl2.h"
+#include <stdio.h>
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+#include "GLFW/glfw3.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 
 #ifdef _WIN32
 #define clear system("cls");
@@ -9,7 +23,89 @@
 #define clear system("clear");
 #endif
 
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+
 int main()
 {
-	std::cout << GenPass(32);
+    // variables
+    char passbuf[1024] = "";
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // This enables vsync so we dont render frames we dont need
+
+    // Setup ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark(); // you can also use ImGui::StyleColorsClassic();
+
+
+    // Choose backend
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+
+    // Definitions
+    bool show_main_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f); // backbuffer color
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents(); // Poll and handle events (inputs, window resize, etc.)
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Gui stuff
+        if (show_main_window)
+        {
+            ImGui::Begin("Main window", &show_main_window);
+
+            ImGui::InputInt("length", &length);
+            if (ImGui::Button("Generate"))
+            {
+                std::string pass;
+                pass = GenPass(length);
+                strcpy(passbuf, pass.c_str());
+            }
+            ImGui::Text("Password:");
+            ImGui::SameLine();
+            ImGui::InputText("##", passbuf, IM_ARRAYSIZE(passbuf));
+            ImGui::End();
+        }
+
+        // Rendering   
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
--- IMPass/passgen.cpp
+++ IMPass/passgen.cpp
@@ -1,26 +1,29 @@
 #include "passgen.h"
 #include "uptime.h"
+#include <iostream>
 #include <chrono>
 
-int seed;
+long long int seed;
 std::string pass;
 
 const int arrSize = 73;
 char charArray[arrSize] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','"','#','%','&','/','(',')','=','?','+' };
 
-int timeSinceEpochNanosec() {
+long long int timeSinceEpochNanosec() {
 	using namespace std::chrono;
 	return duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
 }
 
 int CalcSeed()
 {
-	seed = timeSinceEpochNanosec() + getUptime();
+	seed = timeSinceEpochNanosec() - getUptime();
+	std::cout << seed << std::endl;
 	return seed;
 }
 
 std::string GenPass(int length)
 {
+	pass = "";
 	for (int i = 0; i < length; i++)
 	{
 		srand(CalcSeed());
